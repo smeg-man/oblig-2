71ce9d7979f04778ac780b0044a647cf7a8075ea
smeg-man <43746460+smeg-man@users.noreply.github.com>
2018-10-03 16:48:34 +0200

    added a image

--- git.php
+++ git.php
@@ -16,6 +16,10 @@
 				</h2>
 				<section>
 					<h3>Git</h3>
+					<figure class="float">
+						<img alt="GitHub commit" src="media/images/git_commit.png" />
+						<figcaption>Min siste commit i GitHub</figcaption>
+					</figure>
 					<p>
 						Git er et versjonskontrollsystem.
 						Et versjonskontrollsystem er et program som holder styr på endringer som blir gjort på en fil. Med git kan man gå tilbake til en eldre versjon av filen hvis man vil.
