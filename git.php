59cd5f48e7f4a92827027144f6ea69ccab853595
smeg-man <43746460+smeg-man@users.noreply.github.com>
2018-10-03 08:53:37 +0200

    upload

---
+++ git.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="nb">
+<head>
+	<title>Git / Github</title>
+	<?php include "common/head.html"; ?>
+	<link rel="stylesheet" href="css/artikkel.css" type="text/css"/>
+</head>
+
+<body>
+	<div id="wrapper">
+		<?php include "common/header.html"; ?>
+		<main id="main_content">
+			<h2>
+				Git / Github
+			</h2>
+			<figure class="float">
+				<img alt="Spinnende hodeskalle og bein" src="media/images/skullgre.gif" />
+				<figcaption>Spinnende hodeskalle og bein</figcaption>
+			</figure>
+			<h3>Git</h3>
+			<p>
+				Git er et versjonskontrollsystem.
+				Et versjonskontrollsystem er et program som holder styr på endringer som blir gjort på en fil.
+				Med git kan man gå tilbake til en eldre versjon av filen hvis man vil.
+			</p>
+			<h3>GitHub</h3>
+			<p>
+				Github er en online tjeneste hvor man kan laste opp filene sine og så har man Git versjonskontroll. 
+			</p>
+			<p>
+				Aenean nec velit dui. Fusce vulputate tincidunt leo ut tristique. Integer tempus nunc lectus, at fringilla sapien faucibus nec. Aenean consequat iaculis arcu, ut consequat leo auctor ut. Cras auctor, ipsum vel porta laoreet, nulla libero ullamcorper orci, eget porttitor diam est ut metus. Aenean ultricies, odio non fermentum lobortis, nunc ex fringilla dolor, vestibulum auctor massa urna in eros. Nam vel commodo urna. Vivamus sit amet massa nec neque lobortis bibendum. Nam consequat fringilla augue et ultricies. Quisque congue mauris eget leo tincidunt tristique. Maecenas interdum dui non quam lobortis fermentum. Mauris tristique vehicula enim, et eleifend nisl aliquet sit amet. Nunc laoreet porta nibh, sit amet blandit justo accumsan sed. Pellentesque tristique ultrices enim ac fermentum. Praesent velit erat, pulvinar feugiat massa ut, consequat malesuada odio.
+			</p>
+		</main>
+		<?php include "common/footer.html"; ?>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="nb">
+<head>
+	<title>Index</title>
+	<?php include "common/head.html"; ?>
+	<link rel="stylesheet" href="css/index.css" type="text/css"/>
+</head>
+
+<body>
+	<div id="wrapper">
+		<?php include "common/header.html"; ?>
+		<main id="main_content">
+			<h2>
+				Lorem ipsom
+			</h2>
+			<figure class="float">
+				<img alt="Spinning skull and bones" src="media/images/skullgre.gif" />
+				<figcaption>Spinning skull and bones</figcaption>
+			</figure>
+			<p>
+				Vestibulum consequat felis et velit imperdiet, eu dignissim odio aliquet. Donec egestas sollicitudin velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut a quam sit amet lorem iaculis lacinia. Sed magna dolor, sodales nec efficitur eget, sollicitudin eu magna. Praesent quis aliquet diam. Nam faucibus augue at sapien elementum varius. Phasellus dictum porttitor nisl ut elementum. Sed et ante et dolor congue placerat. Maecenas porta posuere augue malesuada pellentesque. Nunc ultrices nulla neque, sit amet ultricies sapien scelerisque et. Maecenas ac ligula nec nisi scelerisque pharetra faucibus et elit. Curabitur finibus efficitur velit non scelerisque.
+			</p>
+		</main>
+		<aside id="sidebar">
+			<section>
+				<h2>video</h2>
+				<div class="video_container"><!-- denne diven er nødvendig -->
+					<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/hNuCxUVWUyg?rel=0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				</div>
+			</section>
+			<section>
+				<h2>responsivt webdesign</h2>
+				<ul>
+					<li>
+						<a href="http://www.milli.no/content/hva-er-responsiv-design">
+							artikkel
+						</a>
+					</li>
+					<li>
+						<a href="https://no.wikipedia.org/wiki/Responsiv_nettside">
+							artikkel
+						</a>
+					</li>
+					<li>
+						<a href="https://www.techweb.no/blogg/hva-er-responsivt-design">
+							artikkel
+						</a>
+					</li>
+					<li>
+						<a href="https://www.digi.no/artikler/derfor-velger-nesten-alle-responsiv-webdesign/320517">
+							artikkel
+						</a>
+					</li>
+					<li>
+						<a href="https://www.idium.no/blogg/2015/responsivt-design-hva-og-hvorfor/">
+							artikkel
+						</a>
+					</li>
+				</ul>
+			</section>
+			<section>
+				<h2>git hub</h2>
+				<a href="https://www.google.com/search?q=Github">
+					hyperlink
+				</a>
+			</section>
+		</aside>
+		<?php include "common/footer.html"; ?>
+	</div>
+</body>
+</html>
